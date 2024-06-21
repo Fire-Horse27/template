36d92710c520c76d62d4979e48154ac6f043a738
Fire-Horse27 <firehorse2727@gmail.com>
2024-06-20 23:56:25 -0500

    Initial Commit

---
+++ src/main/java/net/fire_horse27/template/TemplateDataGenerator.java
@@ -0,0 +1,12 @@
+package net.fire_horse27.template;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.server.MinecraftServer;
+
+public class TemplateDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
